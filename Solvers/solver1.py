9adeb78631f0d6c7d3cb927a9f7c17d9eb05a316
夏卓凡 <xiazhuofan@qq.com>
2019-05-11 21:26:52 +0800

    一会开完会写

--- Solvers/solver1.py
+++ Solvers/solver1.py
@@ -9,6 +9,7 @@ class Solver1(object):
         self.opt = opt
         self.train_opt = opt['solver']
         self.data_opt = opt['datasets']
+        self.model_opt = opt['networks']
 
         # Image Properties
         self.img_size = opt['img_size']
@@ -19,9 +20,10 @@ class Solver1(object):
         self.learning_rate_init = self.train_opt['lr_init']
         self.optimizer_type = self.train_opt['optimizer_type'].upper()
         self.learning_rate_decay = self.train_opt['lr_decay']
-        self.decay_steps = self.train_opt['decay_steps']
-        self.save_steps = self.train_opt['save_steps']
+        self.decay_epochs = self.train_opt['decay_epochs']
+        self.save_epochs = self.train_opt['save_epochs']
         self.learning_rate = tf.Variable(self.learning_rate_init, name='learning_rate')
+        self.max_steps = self.train_opt['max_steps']
 
         if self.optimizer_type == 'ADAM':
             self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
@@ -35,10 +37,17 @@ class Solver1(object):
             raise NotImplementedError('Optimizer [%s] not supported!' % self.optimizer_type)
         
         # Dataset Options
+        self.cpu_threads = self.opt['cpu_threads']
         self.train_dir = self.data_opt['train']['dir']
-        self.train_dataset = tf.data.TFRecordDataset([self.train_dir])
+        self.train_dataset = tf.data.TFRecordDataset([self.train_dir], None, None, self.cpu_threads)
+        self.train_dataset = self.train_dataset.map(self._parse_function, self.cpu_threads).map(self._augment, self.cpu_threads)
+        self.train_dataset = self.train_dataset.repeat(-1).shuffle(5000).batch(self.batch_size).prefetch(self.batch_size)
         self.val_dir = self.data_opt['val']['dir']
-        self.val_dataset = tf.data.TFRecordDataset([self.val_dir])
+        self.val_dataset = tf.data.TFRecordDataset([self.val_dir], None, None, self.cpu_threads)
+        self.val_dataset = self.val_dataset.map(self._parse_function, self.cpu_threads).batch(self.batch_size).prefetch(self.batch_size)
+
+        # Model Options
+        self.model = create_model(self.model_opt)
 
     def _parse_function(self, example_proto):
         
@@ -68,8 +77,23 @@ class Solver1(object):
         image_out[image_out < 0.0] = 0.0
 
         return image_out
+    
+    def _augment(self, batch):
+
+        images, labels = batch
+        images = tf.image.random_flip_left_right(images)
+        images = tf.image.random_flip_up_down(images)
+        images = tf.image.random_brightness(images, 127.5)
+        batch = (images, labels)
+
+        return batch
+
+    def _save_checkpoint(self):
+
+        pass
 
     def train(self):
+
         pass
     
     def test(self):
