1a0a5f81912c86efc897a5b85dcc0e5e9abb0e40
Vladimir2506 <xiazhuofan@qq.com>
2019-05-22 23:00:16 +0800

    pre-rep

--- Archs/Model_fcn8_arch.py
+++ Archs/Model_fcn8_arch.py
@@ -1,45 +1,54 @@
-import tensorflow as tf 
+import tensorflow as tf
+import os
 import pdb
+import numpy as np
+#import keras_applications.resnet50 as Resnet50
 
-#include two architecture FCN8  and FCN32
 class Model_fcn8(tf.keras.Model):
 
     '''
         Model_fcn8 :  Fcn8 Architecture for task 2.
     '''
 
-    def __init__(self,num_features=64):
+    def __init__(self, num_features=64, num_nclasses =2):
         
         super(Model_fcn8, self).__init__()
 
         self.num_features = num_features
+        self.nclasses = num_nclasses
+        self.block1= Block1(self.nclasses,self.num_features)
+        self.block2= Block2(self.nclasses,self.num_features)
+        self.block3= Block3(self.nclasses,self.num_features)
         
-        self.block1 = Block1(self.num_features)
-        self.block2 = Block2(self.num_features)
-        self.block3 = Block3(self.num_features)
-        
-        self.up8to16 = tf.keras.layers.Convolution2DTranspose(self.num_features * 8, 4, 2, 'same', activation=None)
-        self.up16to32 = tf.keras.layers.Convolution2DTranspose(self.num_features * 4, 4, 2, 'same', activation=None)
+        self.change_score2 = tf.keras.layers.Conv2DTranspose(self.nclasses, 2, 2, padding='valid', activation=None)
 
-        self.up32to256 = tf.keras.layers.Convolution2DTranspose(self.num_features * 2, 4, 8, 'same', activation=None)
+        self.score_pool4 = tf.keras.layers.Convolution2D(self.nclasses,1, 1, 'same', activation='relu',kernel_initializer='he_normal')
+        self.score4 = tf.keras.layers.Conv2DTranspose(self.nclasses, 2, 2, padding='valid', activation=None)
 
-        self.conv_out1 = tf.keras.layers.Convolution2D(self.num_features, 1, 1, 'same', activation=None)
-        self.conv_out2 = tf.keras.layers.Convolution2D(2, 3, 1, 'same', activation='softmax')
+        self.score_pool3 = tf.keras.layers.Convolution2D(self.nclasses,1, 1, 'same', activation='relu',kernel_initializer='he_normal')
+        self.up1 = tf.keras.layers.Conv2DTranspose(self.nclasses, 8, 8, padding='valid', activation=None)
+        #self.reshape_1 = tf.keras.layers.Reshape((-1,self.nclasses))
+        self.activ_1 = tf.keras.layers.Activation('softmax')
+
 
     def call(self, x, training=True):
+        x3 = self.block1(x)
+        x2 = self.block2(x3)
+        x1 = self.block3(x2)
         
-        x3 = self.block1(x, training)
-        x2 = self.block2(x3, training)
-        x1 = self.block3(x2, training)
+        #x1 = self.fcn32.get_layer('score_fr').output
+        x1 = self.change_score2(x1)
+        #x2 = self.fcn32.get_layer('max_pool4').output
+        x2 = self.score_pool4(x2)
+        x4 = x1 + x2
+        x4 = self.score4(x4)
+        #x3 = self.fcn32.get_layer('max_pool3').output
+        x3 =self.score_pool3(x3)
+        x = x3 + x4
 
-        x1 = self.up8to16(x1)
-        x2 = x1 + x2
-        x2 = self.up16to32(x2)
-        x3 = x2 + x3
-        x3 = self.up32to256(x3)
-
-        x = self.conv_out1(x3)
-        x = self.conv_out2(x)
+        x = self.up1(x)
+        #x = self.reshape_1(x)
+        x = self.activ_1(x)
         
         return x
 
@@ -49,35 +58,42 @@ class Block1(tf.keras.Model):
         Block1 :  Part of Fcn32 Architecture for task 2.
     '''
 
-    def __init__(self, num_features=64):
+    def __init__(self, num_nclasses=2,num_features=64,weight_decay=0.005):
         
         super(Block1, self).__init__()
 
         self.num_features = num_features
+        self.nclasses = num_nclasses
         
-        self.conv1_1 = tf.keras.layers.Convolution2D(self.num_features, 3, 1, 'same', activation='relu')
-        self.conv1_2 = tf.keras.layers.Convolution2D(self.num_features, 3, 1, 'same', activation='relu')
+        self.conv1_1 = tf.keras.layers.Convolution2D(self.num_features, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv1_2 = tf.keras.layers.Convolution2D(self.num_features, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool1 = tf.keras.layers.MaxPooling2D(2, 2, 'same')
-        self.conv2_1 = tf.keras.layers.Convolution2D(self.num_features * 2, 3, 1, 'same', activation='relu')
-        self.conv2_2 = tf.keras.layers.Convolution2D(self.num_features * 2, 3, 1, 'same', activation='relu')
+        self.dropout_1=tf.keras.layers.Dropout(0.5)
+        self.conv2_1 = tf.keras.layers.Convolution2D(self.num_features * 2, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv2_2 = tf.keras.layers.Convolution2D(self.num_features * 2, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool2 = tf.keras.layers.MaxPooling2D(2, 2, 'same')
-        self.conv3_1 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu')
-        self.conv3_2 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu')
-        self.conv3_3 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu')
+        self.dropout_2=tf.keras.layers.Dropout(0.5)
+        self.conv3_1 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv3_2 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv3_3 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool3 = tf.keras.layers.MaxPooling2D(2, 2, 'same',name='max_pool3')
+        self.dropout_3=tf.keras.layers.Dropout(0.5)
 
     def call(self, x, training=True):
 
         x = self.conv1_1(x)
         x = self.conv1_2(x)
         x = self.max_pool1(x)
+        x = self.dropout_1(x,training)
         x = self.conv2_1(x)
         x = self.conv2_2(x)
         x = self.max_pool2(x)
+        x = self.dropout_2(x,training)
         x = self.conv3_1(x)
         x = self.conv3_2(x)
         x = self.conv3_3(x)
         x1 = self.max_pool3(x)
+        x = self.dropout_3(x,training)
         
         return x1
 
@@ -87,23 +103,25 @@ class Block2(tf.keras.Model):
         Block2 :  Part of Fcn32 Architecture for task 2.
     '''
 
-    def __init__(self, num_features=64):
+    def __init__(self, num_nclasses=2,num_features=64,weight_decay=0.005):
         
         super(Block2, self).__init__()
 
         self.num_features = num_features
+        self.nclasses = num_nclasses
         
-        self.conv4_1 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu')
-        self.conv4_2 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu')
-        self.conv4_3 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu')
+        self.conv4_1 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv4_2 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv4_3 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool4 = tf.keras.layers.MaxPooling2D(2, 2, 'same',name='max_pool4')
-        
+        self.dropout_1=tf.keras.layers.Dropout(0.5)
+
     def call(self, x, training=True):
         x = self.conv4_1(x)
         x = self.conv4_2(x)
         x = self.conv4_3(x)
         x = self.max_pool4(x)
-       
+        x = self.dropout_1(x,training)
         return x
 
 class Block3(tf.keras.Model):
@@ -112,22 +130,33 @@ class Block3(tf.keras.Model):
         Block3 :  Part of Fcn32 Architecture for task 2.
     '''
 
-    def __init__(self, num_features=64):
+    def __init__(self, num_nclasses=2,num_features=64,weight_decay=0.005):
         
         super(Block3, self).__init__()
 
         self.num_features = num_features
+        self.nclasses = num_nclasses
 
-        self.conv5_1 = tf.keras.layers.Convolution2D(self.num_features * 16, 3, 1, 'same', activation='relu')
-        self.conv5_2 = tf.keras.layers.Convolution2D(self.num_features * 16, 3, 1, 'same', activation='relu')
-        self.conv5_3 = tf.keras.layers.Convolution2D(self.num_features * 16, 3, 1, 'same', activation='relu')
+        self.conv5_1 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv5_2 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.conv5_3 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool5 = tf.keras.layers.MaxPooling2D(2, 2, 'same')      
+        self.fc6 = tf.keras.layers.Convolution2D(4096, 7, 1, 'same', activation='relu')
+        self.dropout_1=tf.keras.layers.Dropout(0.5)
+        self.fc7 = tf.keras.layers.Convolution2D(4096, 1, 1, 'same', activation='relu')
+        self.dropout_2=tf.keras.layers.Dropout(0.5)
+        self.score_fr = tf.keras.layers.Convolution2D(self.nclasses, 1, 1, 'same', activation='relu',kernel_initializer='he_normal',name = 'score_fr')
         
     def call(self, x, training=True):
         x = self.conv5_1(x)
         x = self.conv5_2(x)
         x = self.conv5_3(x)
         x = self.max_pool5(x)
+        x = self.fc6(x)
+        x = self.dropout_1(x,training)
+        x = self.fc7(x)
+        x = self.dropout_2(x,training)
+        x = self.score_fr(x)
                 
         return x
 
@@ -137,16 +166,18 @@ class Model_fcn32(tf.keras.Model):
         Model_fcn32 :  Fcn32 Architecture for task 2.
     '''
 
-    def __init__(self,num_features=64):
+    def __init__(self, num_features=64, num_nclasses=2):
         
         super(Model_fcn32, self).__init__()
 
         self.num_features = num_features
+        self.nclasses = num_nclasses
         
-        self.block1 = Block1(self.num_features)
-        self.block2 = Block2(self.num_features)
-        self.block3 = Block3(self.num_features)
-        self.score2 = tf.keras.layers.Conv2DTranspose(2, kernel_size=(32, 32), strides=(32, 32), padding='valid', activation=None)
+        self.block1= Block1(self.nclasses,self.num_features)
+        self.block2= Block2(self.nclasses,self.num_features)
+        self.block3= Block3(self.nclasses,self.num_features)
+        self.score2 = tf.keras.layers.Conv2DTranspose(self.nclasses, kernel_size=(32, 32), strides=(32, 32), padding='valid', activation=None)
+        self.reshape_1 = tf.keras.layers.Reshape((-1,self.nclasses))
         self.activ_1 = tf.keras.layers.Activation('softmax')
    
     def get_layer(self, name=None, index=None):
@@ -154,10 +185,19 @@ class Model_fcn32(tf.keras.Model):
 
     def call(self, x, training=True):
 
-        x = self.block1(x, training)
-        x = self.block2(x, training)
-        x = self.block3(x, training)
+        x = self.block1(x,training)
+        x = self.block2(x,training)
+        x = self.block3(x,training)
         x = self.score2(x)
+        x = self.reshape_1(x)
         x = self.activ_1(x)
         
         return x
+
+if __name__ == '__main__':
+
+    tf.enable_eager_execution()
+    m = Model_fcn8()
+    x = tf.convert_to_tensor(np.zeros(shape=[2,128,128,3]))
+    y = m(x, True)
+    pdb.set_trace()
--- Solvers/solver2.py
+++ Solvers/solver2.py
@@ -20,6 +20,7 @@ class Solver2(object):
         # Image Properties
         self.img_size = opt['img_size']
         self.img_channels = opt['img_channels']
+        self.big_size = 1024
 
         # Training Options
         self.batch_size = self.train_opt['batch_size']
@@ -49,7 +50,7 @@ class Solver2(object):
         self.cpu_threads = self.opt['cpu_threads']
         self.train_dir = self.data_opt['train']['dir']
         self.train_dataset = tf.data.TFRecordDataset([self.train_dir], None, None, self.cpu_threads)
-        self.train_dataset = self.train_dataset.map(self._parse_function, self.cpu_threads)
+        self.train_dataset = self.train_dataset.map(self._parse_function, self.cpu_threads).map(self._augment, self.cpu_threads)
         self.train_dataset = self.train_dataset.repeat(-1).shuffle(5000).batch(self.batch_size).prefetch(self.batch_size)
         self.val_dir = self.data_opt['val']['dir']
         self.val_dataset = tf.data.TFRecordDataset([self.val_dir], None, None, self.cpu_threads)
@@ -70,6 +71,7 @@ class Solver2(object):
     def _pixelwise_cross_entropy_loss(self, labels, preds):
 
         pixelwise_bce = tf.keras.losses.binary_crossentropy(labels, preds)
+        pixelwise_bce = pixelwise_bce * tf.squeeze(labels)
         pixelwise_bce = tf.math.reduce_mean(pixelwise_bce)
         
         return pixelwise_bce
@@ -84,15 +86,34 @@ class Solver2(object):
         iou_metric = tf.math.divide(tf.math.reduce_sum(intersection) + eps, tf.math.reduce_sum(union) + eps)
 
         return iou_metric
+    
+    def _dice_metric(self, labels, preds):
+
+        eps = 1e-5
+        preds = tf.math.sign(preds - 0.5) * 0.5 + 0.5
+        num = tf.math.abs(labels * preds)
+        den = labels + preds
+        dice_metric = tf.math.divide(tf.math.reduce_sum(num) + eps, tf.math.reduce_sum(den) + eps) * 2
+
+        return dice_metric
+    
+    def _acc_metric(self, labels, preds):
+
+        acc_metric = tf.keras.metrics.binary_accuracy(labels, preds)
+        acc_metric = acc_metric * tf.squeeze(labels)
+        acc_metric = tf.math.reduce_mean(acc_metric)
+
+        return acc_metric
 
     def metric(self, labels, preds):
 
-        return self._iou_metric(labels, preds)
+        #return self._iou_metric(labels, preds)
+        return self._dice_metric(labels, preds)
 
     def loss(self, labels, preds):
 
-        # return self._dice_loss(labels, preds)
-        return self._pixelwise_cross_entropy_loss(labels, preds)
+        return self._dice_loss(labels, preds)
+        # return self._pixelwise_cross_entropy_loss(labels, preds)
 
     def _parse_function(self, example_proto):
         
@@ -152,6 +173,14 @@ class Solver2(object):
         ckpt = tf.train.Checkpoint(optimizer=self.optimizer, model=self.model, optimizer_step=self.global_step)
         ckpt.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
+    def _combine_sobel_prior(self, images):
+        
+        gray = tf.image.rgb_to_grayscale(images)
+        edges = tf.image.sobel_edges(gray)
+        edges = tf.squeeze(edges)
+
+        return tf.concat([images, edges], axis=3)
+
     def train(self):
         
         print('===> Training starts.')
@@ -171,38 +200,38 @@ class Solver2(object):
             background_labels = labels
             foreground_labels = 1 - labels
             
-            images = self._normalize_image(images)            
+            images = self._normalize_image(images)
 
             with tf.GradientTape() as tape:
                 preds = self.model(images, True)
                 foreground_preds, background_preds = preds[:,:,:,0:1], preds[:,:,:,1:]
-                loss = self.loss(foreground_labels, foreground_preds) * 0.9 + self.loss(background_labels, background_preds) * 0.1
+                loss = self.loss(foreground_labels, foreground_preds) + self.loss(background_labels, background_preds)
             
-            train_fg_iou = self.metric(foreground_labels, foreground_preds)
-            train_bg_iou = self.metric(background_labels, background_preds)
+            train_fg_dice = self.metric(foreground_labels, foreground_preds)
+            train_bg_dice = self.metric(background_labels, background_preds)
             
             grads = tape.gradient(loss, self.model.variables)
             
             self.optimizer.apply_gradients(zip(grads, self.model.variables), global_step=self.global_step)
             
-            print('[Step %d] Training loss = %.4f, Training ForegroundIOU = %.2f, Training BackgroundIOU = %.2f.' % (self.global_step, loss, train_fg_iou, train_bg_iou), end='\r')
+            print('[Step %d] Training loss = %.4f, Training ForegroundDice = %.2f, Training BackgroundDice = %.2f.' % (self.global_step, loss, train_fg_dice, train_bg_dice), end='\r')
             
             if glsp % self.eval_steps == 0:
                 
                 print('\n')
-                val_fg_iou = []
-                val_bg_iou = []
+                val_fg_dice = []
+                val_bg_dice = []
                 for val_img, val_lbl in self.val_dataset:
                     
                     val_img = self._normalize_image(val_img)
                     val_prd = self.model(val_img, False)
-                    val_fg_iou.append(self.metric(1 - val_lbl, val_prd[:,:,:,0:1]).numpy())
-                    val_bg_iou.append(self.metric(val_lbl, val_prd[:,:,:,1:]).numpy())
+                    val_fg_dice.append(self.metric(1 - val_lbl, val_prd[:,:,:,0:1]).numpy())
+                    val_bg_dice.append(self.metric(val_lbl, val_prd[:,:,:,1:]).numpy())
 
-                val_fg_iou = np.array(val_fg_iou).mean()
-                val_bg_iou = np.array(val_bg_iou).mean()
+                val_fg_dice = np.array(val_fg_dice).mean()
+                val_bg_dice = np.array(val_bg_dice).mean()
 
-                print('Validation ForegroundIOU = %.2f, Validation BackgroundIOU = %.2f.' % (val_fg_iou, val_bg_iou))
+                print('Validation ForegroundDice = %.2f, Validation BackgroundDice = %.2f.' % (val_fg_dice, val_bg_dice))
 
             if glsp % self.save_steps == 0:
                 self._save_checkpoint()
@@ -215,29 +244,39 @@ class Solver2(object):
     def _load_raw_image(self, path):
 
         img = cv2.imread(path)
-        img = cv2.resize(img, (self.img_size, self.img_size))
+        img = cv2.resize(img, (self.big_size, self.big_size))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        nh, nw = self.big_size // self.img_size, self.big_size // self.img_size
         oshape = img.shape
         img = tf.convert_to_tensor(img)
-        img = tf.reshape(img, [1, self.img_size, self.img_size, self.img_channels])
-        img = tf.cast(img, tf.float32)
+        imgs = []
+        for i in range(nh):
+            for j in range(nw):
+                imgs.append(img[i * self.img_size:(i + 1) * self.img_size, j * self.img_size:(j + 1) * self.img_size,:])
+        imgs = tf.stack(imgs)
+        imgs = tf.cast(imgs, tf.float32)
 
-        return img, oshape
+        return imgs, oshape
     
     def _load_raw_label(self, path):
 
         lbl = cv2.imread(path)
-        lbl = cv2.resize(lbl, (self.img_size, self.img_size))
+        lbl = cv2.resize(lbl, (self.big_size, self.big_size))
+        nh, nw = self.big_size // self.img_size, self.big_size // self.img_size
         if lbl.shape[2] > 3:
             lbl = cv2.cvtColor(lbl, cv2.COLOR_BGRA2BGR)
         lbl = cv2.cvtColor(lbl, cv2.COLOR_BGR2GRAY)
         lbl[lbl > 127.5] = 255.0
         lbl[lbl < 127.5] = 0.0
-        lbl = tf.convert_to_tensor(lbl)
-        lbl = tf.reshape(lbl, [1, self.img_size, self.img_size, 1])
-        lbl = tf.cast(lbl, tf.float32)
+        lbls = []
+        lbl = np.reshape(lbl, [self.big_size, self.big_size, 1])
+        for i in range(nh):
+            for j in range(nw):
+                lbls.append(lbl[i * self.img_size:(i + 1) * self.img_size, j * self.img_size:(j + 1) * self.img_size,:])
+        lbls = tf.stack(lbls)
+        lbls = tf.cast(lbls, tf.float32)
 
-        return lbl
+        return lbls
 
     def _show_image(self, img_tensor):
 
@@ -251,22 +290,16 @@ class Solver2(object):
         plt.imshow(img)
         plt.show()
     
-    def _show_all(self, image_tensor, label_tensor, pred_tensor):
-
-        h, w, c = self.img_size, self.img_size, self.img_channels
-
-        image_tensor = tf.reshape(image_tensor, [h, w, c])
-        label_tensor = tf.reshape(label_tensor, [h, w, 1])
-        pred_tensor = tf.reshape(pred_tensor, [h, w, 1])
-
-        img = image_tensor.numpy().astype(np.uint8)
-        lbl = label_tensor.numpy().astype(np.uint8)
-        prd = pred_tensor.numpy().astype(np.uint8)
+    def _show_all(self, img, lbl, prd):
+        
+        lbl = lbl.astype(np.uint8)
+        prd = prd.astype(np.uint8)
+        img = img.astype(np.uint8)
 
         lbl = cv2.cvtColor(lbl, cv2.COLOR_GRAY2RGB)
         prd = cv2.cvtColor(prd, cv2.COLOR_GRAY2RGB)
 
-        plt.figure(figsize=(12, 6))
+        plt.figure(figsize=(10, 6))
         plt.subplot(131)
         plt.imshow(img)
         plt.subplot(132)
@@ -281,6 +314,20 @@ class Solver2(object):
         hard_map = tf.image.convert_image_dtype(hard_map, tf.uint8, True)
 
         return hard_map
+    
+    def _reconstruct_image(self, images):
+
+        nh, nw = self.big_size // self.img_size, self.big_size // self.img_size
+
+        img = images.numpy()
+
+        reconstructed = np.zeros([self.big_size, self.big_size, images.shape[-1]])
+
+        for i in range(nh):
+            for j in range(nw):
+                reconstructed[i * self.img_size:(i + 1) * self.img_size, j * self.img_size:(j + 1) * self.img_size,:] = img[i * nw + j,:,:,:]
+        
+        return reconstructed
 
     def test(self):
 
@@ -309,22 +356,27 @@ class Solver2(object):
             pred = self.model(image, False)
             pred_fg = pred[:,:,:,0:1]
 
-            iou_fg = self.metric(1 - label, pred_fg)
+            dice_fg = self.metric(1 - label, pred_fg)
             
             image = self._denormalize_image(image)
             prediction = self._hard_predict(1.0 - pred_fg)
+            
+            image = self._reconstruct_image(image)
+            label = self._reconstruct_image(label)
+            prediction = self._reconstruct_image(prediction)
+
             self._show_all(image, label, prediction)
             
-            iou_fg = iou_fg.numpy()
+            dice_fg = dice_fg.numpy()
 
-            test_acc.append(iou_fg)
+            test_acc.append(dice_fg)
 
             prog = idx / len(test_fns) * 100
-            print('[%dth Sample] TestingIOU = %.2f, Progress = %.2f%%.' % ((idx + 1), iou_fg, prog), end='\r')
+            print('[%dth Sample] TestingDice = %.2f, Progress = %.2f%%.' % ((idx + 1), dice_fg, prog), end='\r')
         
         test_acc = np.array(test_acc).mean()
         
         print('\n')
-        print('Total IOU = %.2f.' % test_acc)
+        print('Total dice = %.2f.' % test_acc)
         
         print('===> Testing ends.')
