18aa8ba4db5ce380ef880b9c03a0ea1b0e187169
Vladimir2506 <xiazhuofan@qq.com>
2019-05-23 12:19:40 +0800

    quick

--- Archs/Model_fcn8_arch.py
+++ Archs/Model_fcn8_arch.py
@@ -10,12 +10,12 @@ class Model_fcn8(tf.keras.Model):
         Model_fcn8 :  Fcn8 Architecture for task 2.
     '''
 
-    def __init__(self, num_features=64, num_nclasses =2):
+    def __init__(self,num_features=64):
         
         super(Model_fcn8, self).__init__()
 
         self.num_features = num_features
-        self.nclasses = num_nclasses
+        self.nclasses = 2
         self.block1= Block1(self.nclasses,self.num_features)
         self.block2= Block2(self.nclasses,self.num_features)
         self.block3= Block3(self.nclasses,self.num_features)
@@ -27,14 +27,14 @@ class Model_fcn8(tf.keras.Model):
 
         self.score_pool3 = tf.keras.layers.Convolution2D(self.nclasses,1, 1, 'same', activation='relu',kernel_initializer='he_normal')
         self.up1 = tf.keras.layers.Conv2DTranspose(self.nclasses, 8, 8, padding='valid', activation=None)
-        #self.reshape_1 = tf.keras.layers.Reshape((-1,self.nclasses))
+        self.reshape_1 = tf.keras.layers.Reshape((-1,self.nclasses))
         self.activ_1 = tf.keras.layers.Activation('softmax')
 
 
     def call(self, x, training=True):
-        x3 = self.block1(x)
-        x2 = self.block2(x3)
-        x1 = self.block3(x2)
+        x3 = self.block1(x,training)
+        x2 = self.block2(x3,training)
+        x1 = self.block3(x2,training)
         
         #x1 = self.fcn32.get_layer('score_fr').output
         x1 = self.change_score2(x1)
@@ -68,32 +68,32 @@ class Block1(tf.keras.Model):
         self.conv1_1 = tf.keras.layers.Convolution2D(self.num_features, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.conv1_2 = tf.keras.layers.Convolution2D(self.num_features, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool1 = tf.keras.layers.MaxPooling2D(2, 2, 'same')
-        self.dropout_1=tf.keras.layers.Dropout(0.5)
+        #self.dropout_1=tf.keras.layers.Dropout(0.5)
         self.conv2_1 = tf.keras.layers.Convolution2D(self.num_features * 2, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.conv2_2 = tf.keras.layers.Convolution2D(self.num_features * 2, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool2 = tf.keras.layers.MaxPooling2D(2, 2, 'same')
-        self.dropout_2=tf.keras.layers.Dropout(0.5)
+       # self.dropout_2=tf.keras.layers.Dropout(0.5)
         self.conv3_1 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.conv3_2 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.conv3_3 = tf.keras.layers.Convolution2D(self.num_features * 4, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool3 = tf.keras.layers.MaxPooling2D(2, 2, 'same',name='max_pool3')
-        self.dropout_3=tf.keras.layers.Dropout(0.5)
+        #self.dropout_3=tf.keras.layers.Dropout(0.5)
 
     def call(self, x, training=True):
 
         x = self.conv1_1(x)
         x = self.conv1_2(x)
         x = self.max_pool1(x)
-        x = self.dropout_1(x,training)
+       # x = self.dropout_1(x,training)
         x = self.conv2_1(x)
         x = self.conv2_2(x)
         x = self.max_pool2(x)
-        x = self.dropout_2(x,training)
+       # x = self.dropout_2(x,training)
         x = self.conv3_1(x)
         x = self.conv3_2(x)
         x = self.conv3_3(x)
         x1 = self.max_pool3(x)
-        x = self.dropout_3(x,training)
+       # x = self.dropout_3(x,training)
         
         return x1
 
@@ -114,14 +114,14 @@ class Block2(tf.keras.Model):
         self.conv4_2 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.conv4_3 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool4 = tf.keras.layers.MaxPooling2D(2, 2, 'same',name='max_pool4')
-        self.dropout_1=tf.keras.layers.Dropout(0.5)
+        #self.dropout_1=tf.keras.layers.Dropout(0.5)
 
     def call(self, x, training=True):
         x = self.conv4_1(x)
         x = self.conv4_2(x)
         x = self.conv4_3(x)
         x = self.max_pool4(x)
-        x = self.dropout_1(x,training)
+        #x = self.dropout_1(x,training)
         return x
 
 class Block3(tf.keras.Model):
@@ -142,9 +142,9 @@ class Block3(tf.keras.Model):
         self.conv5_3 = tf.keras.layers.Convolution2D(self.num_features * 8, 3, 1, 'same', activation='relu',kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
         self.max_pool5 = tf.keras.layers.MaxPooling2D(2, 2, 'same')      
         self.fc6 = tf.keras.layers.Convolution2D(4096, 7, 1, 'same', activation='relu')
-        self.dropout_1=tf.keras.layers.Dropout(0.5)
+        #self.dropout_1=tf.keras.layers.Dropout(0.5)
         self.fc7 = tf.keras.layers.Convolution2D(4096, 1, 1, 'same', activation='relu')
-        self.dropout_2=tf.keras.layers.Dropout(0.5)
+        #self.dropout_2=tf.keras.layers.Dropout(0.5)
         self.score_fr = tf.keras.layers.Convolution2D(self.nclasses, 1, 1, 'same', activation='relu',kernel_initializer='he_normal',name = 'score_fr')
         
     def call(self, x, training=True):
@@ -153,9 +153,9 @@ class Block3(tf.keras.Model):
         x = self.conv5_3(x)
         x = self.max_pool5(x)
         x = self.fc6(x)
-        x = self.dropout_1(x,training)
+        #x = self.dropout_1(x,training)
         x = self.fc7(x)
-        x = self.dropout_2(x,training)
+        #x = self.dropout_2(x,training)
         x = self.score_fr(x)
                 
         return x
@@ -166,12 +166,12 @@ class Model_fcn32(tf.keras.Model):
         Model_fcn32 :  Fcn32 Architecture for task 2.
     '''
 
-    def __init__(self, num_features=64, num_nclasses=2):
+    def __init__(self, num_features=64):
         
         super(Model_fcn32, self).__init__()
 
         self.num_features = num_features
-        self.nclasses = num_nclasses
+        self.nclasses = 2
         
         self.block1= Block1(self.nclasses,self.num_features)
         self.block2= Block2(self.nclasses,self.num_features)
@@ -189,7 +189,7 @@ class Model_fcn32(tf.keras.Model):
         x = self.block2(x,training)
         x = self.block3(x,training)
         x = self.score2(x)
-        x = self.reshape_1(x)
+        #x = self.reshape_1(x)
         x = self.activ_1(x)
         
         return x
